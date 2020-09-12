cdd07b113a4b52cc457f1fe9705596c239ad9201
Joshua <psyjr4@nottingham.ac.uk>
2020-09-12 11:49:52 +0100

    Updated lambda to write connection Id to table

--- OnConnectionLambda/src/OnConnectionLambda/Function.cs
+++ OnConnectionLambda/src/OnConnectionLambda/Function.cs
@@ -1,7 +1,9 @@
-using System;
 using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Extensions.NETCore.Setup;
+using Microsoft.Extensions.DependencyInjection;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -10,6 +12,21 @@ namespace OnConnectionLambda
 {
     public class Function
     {
+
+        private ServiceCollection _serviceCollection;
+        
+        public Function()
+        {
+            ConfigureServices();
+        }
+
+        public void ConfigureServices()
+        {
+            _serviceCollection = new ServiceCollection();
+            _serviceCollection.AddDefaultAWSOptions(new AWSOptions());
+            _serviceCollection.AddAWSService<IAmazonDynamoDB>();
+            _serviceCollection.AddTransient<Handler>();
+        }
         
         /// <summary>
         /// A simple function that takes a string and does a ToUpper
@@ -19,13 +36,11 @@ namespace OnConnectionLambda
         /// <returns></returns>
         public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
         {
-            Console.WriteLine("Connected");
-            
-            return new APIGatewayProxyResponse
+
+            using (var serviceProvider = _serviceCollection.BuildServiceProvider())
             {
-                StatusCode = 200,
-                Body = "",
-            };
+                return await serviceProvider.GetService<Handler>().Handle(request);
+            }
         }
     }
 }
---
+++ OnConnectionLambda/src/OnConnectionLambda/Handler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Lambda.APIGatewayEvents;
+
+namespace OnConnectionLambda
+{
+    public class Handler
+    {
+        IAmazonDynamoDB _dynamoDb;
+        const string TableName = "MessagingConnectionsTable";
+
+        public Handler(IAmazonDynamoDB dynamoDb)
+        {
+            _dynamoDb = dynamoDb;
+        }
+        
+        public async Task<APIGatewayProxyResponse> Handle(APIGatewayProxyRequest request)
+        {
+            Console.WriteLine("Connected");
+
+            Console.WriteLine(request.RequestContext.ConnectionId);
+
+            var connectionId = request.RequestContext.ConnectionId;
+
+            var putItemRequest = new PutItemRequest
+            {
+                TableName = TableName,
+                Item = new Dictionary<string, AttributeValue>
+                {
+                    ["UserId"] = new AttributeValue {S = Guid.NewGuid().ToString()},
+                    ["ConnectionId"] = new AttributeValue {S = connectionId},
+                },
+            };
+
+            await _dynamoDb.PutItemAsync(putItemRequest);
+
+            return new APIGatewayProxyResponse
+            {
+                StatusCode = 200,
+                Body = "",
+            };
+        }
+    }
+}
