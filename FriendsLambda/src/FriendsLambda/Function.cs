a5302ad1bd338674b5df0b9bc896012fa53402cc
Joshua <psyjr4@nottingham.ac.uk>
2020-09-17 14:53:35 +0100

    Added a new lambda to get friends from a table

---
+++ FriendsLambda/src/FriendsLambda/Function.cs
@@ -0,0 +1,46 @@
+using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Amazon.Extensions.NETCore.Setup;
+using Microsoft.Extensions.DependencyInjection;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace FriendsLambda
+{
+    public class Function
+    {
+
+        private ServiceCollection _serviceCollection;
+        
+        public Function()
+        {
+            ConfigureServices();
+        }
+
+        public void ConfigureServices()
+        {
+            _serviceCollection = new ServiceCollection();
+            _serviceCollection.AddDefaultAWSOptions(new AWSOptions());
+            _serviceCollection.AddAWSService<IAmazonDynamoDB>();
+            _serviceCollection.AddTransient<Handler>();
+        }
+        
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="request"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+
+            await using (var serviceProvider = _serviceCollection.BuildServiceProvider())
+            {
+                return await serviceProvider.GetService<Handler>().Handle(request);
+            }
+        }
+    }
+}
--- FriendsLambda/src/FriendsLambda/Handler.cs
+++ FriendsLambda/src/FriendsLambda/Handler.cs
@@ -2,17 +2,16 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
+using Amazon.ApiGatewayManagementApi;
+using Amazon.ApiGatewayManagementApi.Model;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda.APIGatewayEvents;
 using Newtonsoft.Json;
-using Amazon.ApiGatewayManagementApi;
-using Amazon.ApiGatewayManagementApi.Model;
 
-namespace OnConnectionLambda
+namespace FriendsLambda
 {
     public class Handler
     {
@@ -26,11 +25,6 @@ namespace OnConnectionLambda
         
         public async Task<APIGatewayProxyResponse> Handle(APIGatewayProxyRequest request)
         {
-            Console.WriteLine("Connected");
-
-            Console.WriteLine(request.RequestContext.ConnectionId);
-
-            var connectionId = request.RequestContext.ConnectionId;
             
             var getUserIdRequest = new GetItemRequest
             {
@@ -53,18 +47,40 @@ namespace OnConnectionLambda
 
             var userId = userIdResponse.Item["UserId"].S;
             
-            var putItemRequest = new PutItemRequest
+            var friendGetRequest = new GetItemRequest
             {
-                TableName = TableName,
-                Item = new Dictionary<string, AttributeValue>
+                TableName = "MessagingUserTable",
+                Key = new Dictionary<string, AttributeValue>
                 {
-                    ["UserId"] = new AttributeValue {S = userId},
-                    ["ConnectionId"] = new AttributeValue {S = connectionId},
-                },
+                    ["UserId"] = new AttributeValue{S = userId}
+                }
             };
 
-            await _dynamoDb.PutItemAsync(putItemRequest);
+            var friendItemResponse = await _dynamoDb.GetItemAsync(friendGetRequest);
+            if (!friendItemResponse.IsItemSet)
+                return forbiddenApiGatewayResponse;
 
+            var friends = friendItemResponse.Item["Connections"].SS;
+            Console.WriteLine(JsonConvert.SerializeObject(friends));
+            
+            
+            var client = new AmazonApiGatewayManagementApiClient(new AmazonApiGatewayManagementApiConfig
+            {
+                ServiceURL = "https://oc9gdrsfcl.execute-api.eu-west-2.amazonaws.com/messaging-test",
+            });
+            
+            var messageBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(friends));
+            var messageStream = new MemoryStream(messageBytes);
+            
+            Console.WriteLine("Hello");
+            var response = await client.PostToConnectionAsync(new PostToConnectionRequest
+            {
+                ConnectionId = request.RequestContext.ConnectionId,
+                Data = messageStream,
+            }, CancellationToken.None);
+            
+            Console.WriteLine(JsonConvert.SerializeObject(response));
+            
             return new APIGatewayProxyResponse
             {
                 StatusCode = 200,
