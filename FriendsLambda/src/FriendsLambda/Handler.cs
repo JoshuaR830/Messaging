13f8c3bc6eeada839cc668cdefdb941632da2295
Joshua <psyjr4@nottingham.ac.uk>
2020-09-17 17:06:16 +0100

    Lambda will now be invoked by REST API

--- FriendsLambda/src/FriendsLambda/Handler.cs
+++ FriendsLambda/src/FriendsLambda/Handler.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-using Amazon.ApiGatewayManagementApi;
-using Amazon.ApiGatewayManagementApi.Model;
 using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.Model;
 using Amazon.Lambda.APIGatewayEvents;
@@ -63,28 +58,28 @@ namespace FriendsLambda
             var friends = friendItemResponse.Item["Connections"].SS;
             Console.WriteLine(JsonConvert.SerializeObject(friends));
             
-            
-            var client = new AmazonApiGatewayManagementApiClient(new AmazonApiGatewayManagementApiConfig
-            {
-                ServiceURL = "https://oc9gdrsfcl.execute-api.eu-west-2.amazonaws.com/messaging-test",
-            });
-            
-            var messageBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(friends));
-            var messageStream = new MemoryStream(messageBytes);
-            
-            Console.WriteLine("Hello");
-            var response = await client.PostToConnectionAsync(new PostToConnectionRequest
-            {
-                ConnectionId = request.RequestContext.ConnectionId,
-                Data = messageStream,
-            }, CancellationToken.None);
-            
-            Console.WriteLine(JsonConvert.SerializeObject(response));
+            //
+            // var client = new AmazonApiGatewayManagementApiClient(new AmazonApiGatewayManagementApiConfig
+            // {
+            //     ServiceURL = "https://oc9gdrsfcl.execute-api.eu-west-2.amazonaws.com/messaging-test",
+            // });
+            //
+            // var messageBytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(friends));
+            // var messageStream = new MemoryStream(messageBytes);
+            //
+            // Console.WriteLine("Hello");
+            // var response = await client.PostToConnectionAsync(new PostToConnectionRequest
+            // {
+            //     ConnectionId = request.RequestContext.ConnectionId,
+            //     Data = messageStream,
+            // }, CancellationToken.None);
+            //
+            // Console.WriteLine(JsonConvert.SerializeObject(response));
             
             return new APIGatewayProxyResponse
             {
                 StatusCode = 200,
-                Body = "",
+                Body = JsonConvert.SerializeObject(friends),
             };
         }
     }
